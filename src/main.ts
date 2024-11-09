fbd77114327622a6e5b252246e3c2b26ee9fac9b
Omar Hamdi <devomarhamdi@gmail.com>
2024-11-09 22:30:35 +0200

    fix: logout

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  app.enableCors({ origin: '*', credentials: true });
+  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
   // for creating the items master data
   app.use(bodyParser.json({ limit: '50mb' }));
   app.setGlobalPrefix('api/');
